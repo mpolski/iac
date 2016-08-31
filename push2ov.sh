51cc02f15c71687d33ed19504b7ad80a124652c5
mpolski <polski@hpe.com>
2016-08-31 14:54:31 +0200

    added push2ov script

---
+++ push2ov.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SERVER=$1
+USER=$2
+PASS=$3
+JSONFILE=$4
+URL="https://$SERVER"
+
+AUTH=`curl -s -k -H "Content-Type: application/json" -d '{"userName":"'$USER'","password":"'$PASS'"}' POST $URL/rest/login-sessions | jq -r .sessionID`
+
+echo $AUTH
+ 
+curl -s -k -H "Content-Type: application/json" -H "Auth: $AUTH" -H "X-Api-Version: 300" -d @$4 -X POST $URL/rest/server-profiles 
