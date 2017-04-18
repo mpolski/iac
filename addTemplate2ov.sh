104501874dd94715318378f12d9507b3defc7e98
mpolski <polski@hpe.com>
2017-04-18 18:49:32 +0200

    initial files

---
+++ addTemplate2ov.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SERVER=$1
+USER=$2
+PASS=$3
+JSONFILE=$4
+URL="https://$SERVER"
+
+AUTH=`curl -s -k -H "X-API-Version: 300" -H "Content-Type: application/json" -d '{"userName":"'$USER'","password":"'$PASS'","loginMsgAck":"true"}' POST $URL/rest/login-sessions | jq -r .sessionID`
+
+echo $AUTH
+ 
+curl -s -k -H "X-API-Version: 300" -H "Content-Type: application/json" -H "Auth: $AUTH" -d @$JSONFILE -X POST $URL/rest/server-profile-templates
